1c04e85914394fec4101ba1264ca18225c5052dc
christianTDConsulting <147801506+christianTDConsulting@users.noreply.github.com>
2023-10-30 13:24:59 +0100

    Delete build directory

--- build/src/database/database.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Prisma = exports.db = void 0;
-const client_1 = require("@prisma/client");
-const client_2 = require("@prisma/client");
-Object.defineProperty(exports, "Prisma", { enumerable: true, get: function () { return client_2.Prisma; } });
-const db = new client_1.PrismaClient();
-exports.db = db;
